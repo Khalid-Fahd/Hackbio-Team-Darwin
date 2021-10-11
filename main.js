309f427320ffd938ffdba0b1318f43b5b0e862a3
Ishbah <88969686+Ishbah@users.noreply.github.com>
2021-10-12 02:14:56 +0530

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-const myname="Ishbah Hilal";
-document.getElementById("name").innerHTML = myname;
-const email="ishi.hilal@gmail.com";
-document.getElementById("email").innerHTML = email;
-const discord="@Sapphire#7008";
-document.getElementById("discord").innerHTML = discord;
-const Biostack="Data Analytics";
-document.getElementById("Biostack").innerHTML = Biostack;
-
