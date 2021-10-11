3e359972592a5cc3c8da93a1a8c363c428dcdcd9
Ishbah <88969686+Ishbah@users.noreply.github.com>
2021-10-12 02:21:01 +0530

    Add files via upload

---
+++ T1Ishbah_main.js
@@ -0,0 +1,9 @@
+const myname="Ishbah Hilal";
+document.getElementById("name").innerHTML = myname;
+const email="ishi.hilal@gmail.com";
+document.getElementById("email").innerHTML = email;
+const discord="@Sapphire#7008";
+document.getElementById("discord").innerHTML = discord;
+const Biostack="Data Analytics";
+document.getElementById("Biostack").innerHTML = Biostack;
+
