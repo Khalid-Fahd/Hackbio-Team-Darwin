b5b347226f4aaf52596b8e8bbf9915ced3513cfb
JEMINA-JOHN <74172716+JEMINA-JOHN@users.noreply.github.com>
2021-10-11 23:54:12 +0530

    Add files via upload

---
+++ JEMINA J_HACKBIO_TASK 1.pl
@@ -0,0 +1,5 @@
+$name = 'JEMINA J';
+$email = 'j.jemina2002@gmail.com';
+$discord = '@JEMINA_JOHN#3351';
+$Biostack = 'Environmental Bioinformatics';
+print "Name : $name\nE-mail : $email\nDiscord Username : $discord\nBiostack : $Biostack\n";
