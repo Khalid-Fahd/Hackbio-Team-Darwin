1cd7d99043c282453b0aa460e20721178816e7a4
JEMINA-JOHN <74172716+JEMINA-JOHN@users.noreply.github.com>
2021-10-11 23:31:43 +0530

    Create JEMINA J_TASK 1.pl
    This is my Perl file

---
+++ JEMINA J_TASK 1.pl
@@ -0,0 +1,5 @@
+$name = 'JEMINA J';
+$email = 'j.jemina2002@gmail.com';
+$discord = '@JEMINA_JOHN';
+$Biostack = 'Environmental Bioinformatics';
+print "Name : $name\nE-mail : $email\nDiscord Username : $discord\nBiostack : $Biostack\n";
