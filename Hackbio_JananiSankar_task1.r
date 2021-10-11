452ef84f5ad9509ebd87c050329254c39f8879e5
Jananisan95 <92268938+Jananisan95@users.noreply.github.com>
2021-10-11 11:33:33 -0600

    Add Janani first solution

---
+++ Hackbio_JananiSankar_task1.r
@@ -0,0 +1,12 @@
+ personal_info <- function(){
+  Name = "Janani Sankar"
+   Email = "jananisan@gmail.com"
+   Discord_username = "@JananiSankar#9691"
+   Biostack = "Vaccine informatics!!!"
+   
+   cat(paste('',"Name:",Name,'\n',
+             "Email:",Email,'\n',
+             'Discord_userName:',Discord_username,'\n',
+             'Biostack:',Biostack,'\n'))
+   }
+ personal_info()
