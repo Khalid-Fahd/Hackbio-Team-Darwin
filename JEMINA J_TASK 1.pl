dc6e54c412e172038ad59e3321ab13d2f14bfb87
JEMINA-JOHN <74172716+JEMINA-JOHN@users.noreply.github.com>
2021-10-11 23:38:49 +0530

    Update JEMINA J_TASK 1.pl

--- JEMINA J_TASK 1.pl
+++ JEMINA J_TASK 1.pl
@@ -1,5 +1,5 @@
 $name = 'JEMINA J';
 $email = 'j.jemina2002@gmail.com';
-$discord = '@JEMINA_JOHN';
+$discord = '@JEMINA_JOHN#3351';
 $Biostack = 'Environmental Bioinformatics';
 print "Name : $name\nE-mail : $email\nDiscord Username : $discord\nBiostack : $Biostack\n";
