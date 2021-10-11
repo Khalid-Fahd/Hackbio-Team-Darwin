e70d5b214fcf68abfaa368550d6e52309e1764fd
JEMINA-JOHN <74172716+JEMINA-JOHN@users.noreply.github.com>
2021-10-11 23:55:06 +0530

    Delete JEMINA J_TASK 1.pl

--- JEMINA J_TASK 1.pl
+++
@@ -1,5 +0,0 @@
-$name = 'JEMINA J';
-$email = 'j.jemina2002@gmail.com';
-$discord = '@JEMINA_JOHN#3351';
-$Biostack = 'Environmental Bioinformatics';
-print "Name : $name\nE-mail : $email\nDiscord Username : $discord\nBiostack : $Biostack\n";
