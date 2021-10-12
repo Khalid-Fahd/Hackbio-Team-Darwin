c606ca55da340921fc990ba7412d32605949b16a
Ishbah <88969686+Ishbah@users.noreply.github.com>
2021-10-12 18:32:05 +0530

    Update T1Ishbah_main.js

--- T1Ishbah_main.js
+++ T1Ishbah_main.js
@@ -2,8 +2,8 @@ const myname="Ishbah Hilal";
 document.getElementById("name").innerHTML = myname;
 const email="ishi.hilal@gmail.com";
 document.getElementById("email").innerHTML = email;
-const discord="@Sapphire#7008";
-document.getElementById("discord").innerHTML = discord;
-const Biostack="Data Analytics";
+const slack="@Sapphire0️⃣";
+document.getElementById("slack").innerHTML = slack;
+const Biostack="Machine Learning and Data Science";
 document.getElementById("Biostack").innerHTML = Biostack;
 
