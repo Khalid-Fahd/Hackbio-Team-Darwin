c6474018b87572fa86f0688e00b838ae7eefff63
Farheen <sfar0315@gmail.com>
2021-10-12 10:39:58 -0400

    Create Farheen_Shah_task1.R

---
+++ Farheen_Shah_task1.R
@@ -0,0 +1,17 @@
+#  HackBio task 1
+# This is a script in R that prints name, email, discord username with @ and biostack
+
+# Storing name, email, username and biostack as variables and then printing them
+
+
+name <- "Farheen"
+email <- "shaikhfarheen03@gmail.com"
+username <- "Farheen Shah"
+biostack <- "Genomics"
+
+print (paste ("My name is", name))
+print (paste ("My email is", email))
+print (paste0 ("My slack username is @", username))
+print (paste ("My biostack is", biostack))       
+
+
