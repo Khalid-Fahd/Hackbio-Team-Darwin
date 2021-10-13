87e064cb160f5256f26d71c106970392c18d7cf0
Shalaka More <92265707+shalaka98@users.noreply.github.com>
2021-10-13 22:42:17 +0530

    Update and rename Shalaka_Hackbio_task1_python.py to Shalaka_task1.py

--- Shalaka_Hackbio_task1_python.py
+++
@@ -1,6 +0,0 @@
-name = "shalaka"
-email = "shalakamore18@gmail.com"
-discord = "@shalaka"
-biostack = "genomics"
-
-print( "name:",name + ", email:", email + ", discord:", discord + ", biostack:",biostack)
