a422c5b84cffae59fe50976dd0b25ac84e77771d
JEMINA-JOHN <j.jemina2002@gmail.com>
2021-10-14 18:04:12 +0530

    corrected Joyomonigho
    Header files were missing

--- Joyomonigho.c
+++ Joyomonigho.c
@@ -1,1 +1,6 @@
-printf("joy egbejale beautifulljoyce@gmail.com @joyomonigho genomics"); 
+#include <stdio.h>
+int main()
+{
+printf("Name: joy egbejale\nEmail: beautifulljoyce@gmail.com\nUsername: @joyomonigho\nBiostack: genomics"); 
+return 0;
+}
