7b50ff507e08f68ef7be1136b431e948703b9104
Khalid <55442257+Khalid-Fahd@users.noreply.github.com>
2021-10-14 13:29:50 +0100

    Khalid-Fahd's script
    Python script for Khalid Fahd's details.

---
+++ Khalid-Fahd.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 14 13:16:18 2021
+
+@author: Mac
+"""
+
+name = "Fahd Adebola Khalid-Salako"
+email = "salakofahdabiodun@gmail.com"
+discord_ID = "Khalid#4799"
+Biostack = "Data analytics"
+
+print(name)
+print(email)
+print(discord_ID)
+print(Biostack)
