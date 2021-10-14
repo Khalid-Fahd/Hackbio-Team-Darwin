2b30f280ab397b8eaccd25cce58d4d6609efd0b5
JEMINA-JOHN <74172716+JEMINA-JOHN@users.noreply.github.com>
2021-10-14 19:08:26 +0530

    Delete JEMINA-JOHN.js

--- JEMINA-JOHN.js
+++
@@ -1,8 +0,0 @@
-const Name="JEMINA J";
-document.getElementById("name").innerHTML = Name;
-const email="j.jemina2002@gmail.com";
-document.getElementById("email").innerHTML = email;
-const slack="@JEMINA_JOHN0️⃣";
-document.getElementById("slack").innerHTML = slack;
-const Biostack="Machine Learning and Data Science";
-document.getElementById("Biostack").innerHTML = Biostack;
