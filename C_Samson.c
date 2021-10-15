6dd2074a72b9a91bc7cbd4e597b12f6ff558e107
JEMINA-JOHN <74172716+JEMINA-JOHN@users.noreply.github.com>
2021-10-15 16:41:56 +0530

    Update C_Samson.c

--- C_Samson.c
+++ C_Samson.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 int main(){
-     printf("\nSamson Folami");
-     printf("folami.samson@gmail.com");
-     printf("@Samson");
-     printf("Genomics\n");
+     printf("\nSamson Folami, ");
+     printf("folami.samson@gmail.com, ");
+     printf("@Samson, ");
+     printf("Genomics, \n");
     return(0);
 }
