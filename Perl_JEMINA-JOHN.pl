8c2513dd578ead5da93a04e124171ad3232936e5
JEMINA-JOHN <74172716+JEMINA-JOHN@users.noreply.github.com>
2021-10-15 16:21:11 +0530

    Update Perl_JEMINA-JOHN.pl

--- Perl_JEMINA-JOHN.pl
+++ Perl_JEMINA-JOHN.pl
@@ -2,4 +2,4 @@ $name = 'JEMINA J';
 $email = 'j.jemina2002@gmail.com';
 $slack = '@JEMINA_JOHN';
 $Biostack = 'Machine Learning and Data Science';
-print "$name,$email,$slack,$Biostack\n";
+print "$name\, $email\, $slack\, $Biostack\n";
