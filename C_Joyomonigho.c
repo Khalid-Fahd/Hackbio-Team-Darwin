491a2992e7949d5ca771f9b20ac3029063c89c11
JEMINA-JOHN <74172716+JEMINA-JOHN@users.noreply.github.com>
2021-10-15 16:17:03 +0530

    Update C_Joyomonigho.c

--- C_Joyomonigho.c
+++ C_Joyomonigho.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main()
 {
-printf("Name: joy egbejale\nEmail: beautifulljoyce@gmail.com\nUsername: @joyomonigho\nBiostack: genomics"); 
+printf("\njoy egbejale,beautifulljoyce@gmail.com,@joyomonigho,genomics");
 return 0;
 }
