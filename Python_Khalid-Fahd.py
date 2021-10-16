44d25600cba16595f377fa847495e91d4c879821
Khalid <55442257+Khalid-Fahd@users.noreply.github.com>
2021-10-16 02:59:16 +0100

    Update Python_Khalid-Fahd.py

--- Python_Khalid-Fahd.py
+++ Python_Khalid-Fahd.py
@@ -7,7 +7,7 @@ Created on Thu Oct 14 13:16:18 2021
 
 name = "Fahd Adebola Khalid-Salako"
 email = "salakofahdabiodun@gmail.com"
-discord_ID = "Khalid#4799"
+slack_ID = "@Khalid"
 Biostack = "Data analytics"
 
-print( name + "," + email + "," + discord_ID + "," + Biostack)
+print( name + "," + email + "," + slack_ID + "," + Biostack)
